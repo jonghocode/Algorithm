ba6b7a4fc2ab8e953b33a20c785b89dbe846f375
jonghocode <123474680+jonghocode@users.noreply.github.com>
2023-09-25 01:09:07 +0900

    BFS

--- 백준 1107(리모컨).py
+++ 백준 1107(리모컨).py
@@ -20,13 +20,14 @@ while q:
         continue
     chk[now][sw] = 1 # 뺀 값 체크
     
-    for i in range(10):
-        if i not in lst:
-            if sw == 0: # 처음 들어온 경우는 숫자만 저장
-                    q.append([i, d+1, 1])
-            else:
-                if now*10+i <= 1000011: # 만약 눌려졌었다면 숫자 덮어쓰기
-                    q.append([now*10+i, d+1, 1])
+    if sw == 1 or now == 100: # 이 부분을 안 넣어주니까 메모리 초과 남
+        for i in range(10):
+            if i not in lst:
+                if sw == 0: # 처음 들어온 경우는 숫자만 저장
+                        q.append([i, d+1, 1])
+                else:
+                    if now*10+i <= 1000011: # 만약 눌려졌었다면 숫자 덮어쓰기
+                        q.append([now*10+i, d+1, 1])
                     
             
     
