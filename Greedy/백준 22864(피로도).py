159bd820686807b3cbde528bac5719a8af40f331
jonghocode <whdgh256@naver.com>
2023-10-26 15:53:12 +0900

    Algorithm

---
+++ Greedy/백준 22864(피로도).py
@@ -0,0 +1,15 @@
+a, b, c, d = map(int, input().split())
+
+# answer = 0
+# st = 0
+# for i in range(1, 25):
+#     if st + a <= d:
+#         st += a
+#         answer += b
+#     else:
+#         st -= c
+#         if st < 0:
+#             st = 0
+#     # print(f"i : {i}, st : {st}, answer : {answer}")
+print((24//(d//a + d//c) * (d//a*b))+ (min(24 % (d//a + d//c), d//a)*(b)))
+# print(answer)
