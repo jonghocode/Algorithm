a2655d18de0ad1d2ed1508ca5521703970aa4ff4
jonghocode <123474680+jonghocode@users.noreply.github.com>
2023-10-31 00:29:14 +0900

    Algorithm

--- 구현/프로그래머스 과제 진행하기.py
+++ 구현/프로그래머스 과제 진행하기.py
@@ -1,1 +1,35 @@
-#과제진행하기
+def solution(plans):
+    answer = []
+    
+    for i in range(len(plans)):
+        word = plans[i][1]
+        minute = int(word[:2])*60
+        minute += int(word[3:])
+        plans[i][1] = minute
+        plans[i][2] = int(plans[i][2])
+    
+    plans.sort(key = lambda x : x[1])
+    
+    idx = 1
+    stack = [[plans[0][0], plans[0][1], plans[0][2]]]
+    
+    sw = 0
+    while stack:
+        a, b, c = stack.pop()
+        if idx >= len(plans):
+            sw = 1
+        print(a, b, c, stack)
+        if idx < len(plans):
+            if b + c <= plans[idx][1]:
+                answer.append(a)
+                stack.append([plans[idx][0], plans[idx][1], plans[idx][2]])
+            else:
+                stack.append([a, plans[idx][1], c - (plans[idx][1] - b)])
+                stack.append([plans[idx][0], plans[idx][1], plans[idx][2]])
+    
+        elif idx >= len(plans):
+            answer.append(a)
+        if sw == 0:
+            idx += 1
+    
+    return answer
