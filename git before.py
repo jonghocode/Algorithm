68302fb1abfc1646064eb75987a15ff29b42b9dc
jonghocode <123474680+jonghocode@users.noreply.github.com>
2023-11-26 22:28:38 +0900

    git before.py

--- git before.py
+++ git before.py
@@ -1,1 +1,9 @@
-프로그래머스 순위검색
+클라이언트 키 : 8a2ff1aebc8f8febeb2c9a8a578f5c14
+redirect url : http://localhost:8090/kakao/login (
+카카오 로그인을 하고나서 로그인이 정상적으로 되었다면 이 주소로 정보를 전달 받음)
+
+비밀번호는 디폴트 값이 들어가야 한다?
+
+oauth2 client 라이브러리
+
+https://www.youtube.com/watch?v=NwQ_55l0Za4
