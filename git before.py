c19375d2ce52b98bb7830cb3a6a0032f63a49a63
whdgh256 <whdgh256@naver.com>
2023-11-30 00:43:34 +0900

    Algorithm

--- git before.py
+++ git before.py
@@ -1,1 +1,28 @@
 #이진검색트리
+import sys
+sys.setrecursionlimit(10**6)
+
+lst = []
+while True:
+    try :
+        lst.append(int(sys.stdin.readline()))
+    except:
+        break
+
+def go(temp):
+    if len(temp) == 0:
+        return
+    left, right = [], []
+    root = temp.pop(0)
+    for i in range(len(temp)):
+        if root < temp[i]:
+            right.append(temp[i])
+        elif root > temp[i]:
+            left.append(temp[i])
+    
+    go(left)
+    go(right)
+    print(root)
+
+go(lst)
+print(lst[0])
