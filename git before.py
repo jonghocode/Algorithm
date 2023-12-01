132a453cf6e513e28b5b4e147618c14c4f147e7e
jonghocode <whdgh256@naver.com>
2023-12-01 19:01:18 +0900

    Algorithm

--- git before.py
+++ git before.py
@@ -1,37 +1,35 @@
-# 각 계란의 내구도는 상대의 무게만큼 깎임
-def go(d, sum, idx):
-    global answer
-    # print(f"answer : {answer}, d : {d}, sum : {sum}, idx : {idx}, lst : {lst}")
-    if d == n:
-        answer = max(answer, sum)
-        return
-    
-    for i in range(idx, n): # 오른쪽에 있는 계란 잡기
-        if lst[i][0] > 0:
-            st, weight = lst[i][0], lst[i][1]
-            sw = 0
-            for j in range(n):
-                cnt = 0
-                if i != j and lst[j][0] > 0:
-                    st2, weight2 = lst[j][0], lst[j][1]
-                    lst[i][0] -= weight2
-                    lst[j][0] -= weight
-                    if lst[i][0] <= 0:
-                        cnt += 1
-                    if lst[j][0] <= 0:
-                        cnt += 1
-                    go(d+1, sum+cnt, i+1)
-                    lst[i][0] = st
-                    lst[j][0] = st2
-                    sw = 1
-            if sw == 0: # 고를 계란이 없다면 그냥 넘어가기
-                go(d+1, sum, i+1)
-        else: # 오른쪽에 있는 계란을 잡았는데 깨져있다면 넘어가기
-            go(d+1, sum, i+1)
-            
+# 오등큰수
+
 n = int(input())
-lst = [list(map(int, input().split())) for _ in range(n)]
-chk = [0]*n
-answer = -1
-go(0, 0, 0)
-print(answer)
+lst = list(map(int, input().split()))
+dict = {}
+for i in lst:
+    if i not in dict:
+        dict[i] = 1
+    else:
+        dict[i] += 1
+
+answer = [0]*n
+answer[-1] = -1
+stack = [lst[-1]]
+
+for i in range(n-2, -1, -1):
+    if stack:
+        if dict[stack[-1]] > dict[lst[i]]:
+            answer[i] = stack[-1]
+            stack.append(lst[i])
+        else:
+            while True:
+                if len(stack) == 0 or dict[stack[-1]] > dict[lst[i]]:
+                    break
+                stack.pop()
+            if len(stack) == 0:
+                answer[i] = -1
+            else:
+                answer[i] = stack[-1]
+            stack.append(lst[i])
+    else:
+        answer[i] = -1
+
+for i in range(n):
+    print(answer[i], end=' ')
