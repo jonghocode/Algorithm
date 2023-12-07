624786988bd75cc44ee0ae78fc06ad0308330d95
jonghocode <whdgh256@naver.com>
2023-12-07 15:25:36 +0900

    Algorithm

--- Greedy/백준 12970(AB).py
+++ Greedy/백준 12970(AB).py
@@ -3,24 +3,31 @@ s = []
 for i in range(n):
     s.append('B')
 
-sum, acnt = 0, 0
+sum, acnt, T = 0, 0, n
+flag = False
 for i in range(n):
-    now = 0
-    sw = 0
+    now = -1
+    sw = i
     for j in range(n-1, -1, -1):
         if s[j] == 'A':
             break
         now += 1
-        s[j] = "A"
-        if now != k:
-            s[j] = 'B'
-        else:
-            sw = 1
+        if now + sum == k:
+            sw = j
+            flag = True
             break
-    if sw == 0:
-        s[i] = 'A'
-    acnt += 1
-    sum += now
-    sum -= acnt
-    print(s, sum)
-print(s)
+
+        # print(s, sum, now, j)
+    s[sw] = 'A'
+
+    if now+sum == k:
+        flag = True
+        break
+    acnt += 1 # A개수
+    T -= 1 # B개수
+    sum = T*acnt-acnt # 9 88 777 # A가 하나 더 생길때마다 그 전 A의 개수는 하나 줄어든다.
+    # print(s, sum)
+if flag:
+    print(''.join(map(str, s)))
+else:
+    print(-1)
