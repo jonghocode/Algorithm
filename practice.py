b8f439c3374ddd86bdd54f321164e6595b040410
jonghocode <whdgh256@naver.com>
2023-12-08 19:38:04 +0900

    Algorithm

--- practice.py
+++ practice.py
@@ -1,35 +1,47 @@
 # 로봇 조종하기
-# 제일 높은 수를 박고 시작
-n, left, right = map(int, input().split())
-lst = [1]*n
-max_value = max(left, right)
+# 왼쪽, 오른쪽, 아래로 이동
+# (0,0) -> (n-1, m-1)
 
-if left + right > n + 1: # 예외처리
-    print(-1)
-    exit(0)
+# 다른길로 똑같은 길을 지나갈 때 이미 저장되어있었던
+# 깊이가 4고 현재 깊이는 8이다 그럼 갈 수 있냐?(갈 수 있게 해야함)
 
-lst[n-right] = max_value # 높은 것 먼저 놓기
-cnt = 2
-for i in range(n-2, n-right, -1): # 최댓값까지 오른쪽 값 최소로 놓기
-    lst[i] = cnt
-    cnt += 1
-
-# if b != 1:
-#     lst[n-1] = 1
-# if lst[n-right] > 1:
-#     a -= 1
-# if a == 0 and k1 == 1:
-#     p = lst[n-b]
-#     lst[n-b] = 1
-#     lst[0] = p
-if left == 1:
-    lst[n-right] = 1
-    lst[0] = max_value
-else:
-    temp = left-1
-    for i in range(n-1-right, n-right-left, -1):
-        lst[i] = temp
-        temp -= 1
-
-for i in range(n):
-    print(lst[i], end=' ')
+# 그 전 방향이 아래라면 왼쪽 오른쪽 아래 가능
+# 그 전 방향이 왼쪽이라면 왼쪽, 아래 가능
+# 그 전 방향이 오른쪽이라면 오른쪽, 아래 가능
+# 저장되어있는 값보다 크고 그 전 방향이 아래라면 지나가게
+# 저장되어있는 값보다 크고 그 전 방향이 왼쪽, 오른쪽이라면 못지나가게
+# 1000*1000*1000(n^3)이다 bfs는 경우의수가 너무 많다 시간초과
+# dfs + dp는 풀린다.(시간초과 x)
+# 추가로 위를 향한 방향으로 가지 못하는것은 dp를 의심해볼수있다.
+from heapq import heappush, heappop
+import sys
+n, m = map(int, input().split())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+visit = [[[-1000 for _ in range(m)] for _ in range(n)] for _ in range(3)]
+dirx, diry = [1, 0, 0], [0, -1, 1]
+q = []
+# x, y, dir, cost
+heappush(q, [-board[0][0], 0, 0, 0])
+answer = -987654321
+while q:
+    cost, x, y, dir = heappop(q)
+    if x == n-1 and y == m-1:
+        answer = max(answer, -cost)
+        break
+    if visit[dir][x][y] > -cost:
+        continue
+    for i in range(3):
+        if dir == 1 and i == 2:
+            continue
+        elif dir == 2 and i == 1:
+            continue
+        nx, ny = x + dirx[i], y + diry[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+        if visit[i][nx][ny] > -cost + board[nx][ny]:
+            continue
+        
+        visit[i][nx][ny] = -cost + board[nx][ny]
+        heappush(q, [-(-cost+board[nx][ny]), nx, ny, i])
+    
+print(answer)
