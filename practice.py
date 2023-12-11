632945ad9cdde7274c7c131d8c9f22b7c5b4f6c7
whdgh256 <whdgh256@naver.com>
2023-12-11 23:25:03 +0900

    Algorithm

--- practice.py
+++ practice.py
@@ -5,18 +5,18 @@ n = int(input())
 promax = [] # 최대힙
 promin = [] # 최소힙
 sol = {}
+lst = {}
 for _ in range(n):
     p, l = map(int, sys.stdin.readline().split())
     heappush(promax, [-l, -p])
     heappush(promin, [l, p])
+    lst[str(p)] = str(l)
 
 m = int(input())
 for _ in range(m):
     command = list(sys.stdin.readline().strip().split())
     if command[0] == 'add': # 더하기
-        if command[1] in sol:
-            sol[command[1]] = command[2]
-
+        lst[str(command[1])] = str(command[2])
         heappush(promax, [-int(command[2]), -int(command[1])])
         heappush(promin, [int(command[2]), int(command[1])])
 
@@ -24,8 +24,10 @@ for _ in range(m):
         if command[1] == '1':
             while True: # sol안에 없는 것을 뽑을 때 까지 뽑기
                 level, pro = -promax[0][0], -promax[0][1]
-                if level in sol:
-                    if sol[str(level)] == '0':
+                level = str(level)
+                pro = str(pro)
+                if pro in sol:
+                    if level in sol[pro]:
                         heappop(promax)
                     else:
                         print(pro)
@@ -33,11 +35,15 @@ for _ in range(m):
                 else:
                     print(pro)
                     break
+        
+        
         elif command[1] == '-1':
             while True: # sol안에 없는 것을 뽑을 때 까지 뽑기
                 level, pro = promin[0][0], promin[0][1]
-                if level in sol:
-                    if sol[level] in pro:
+                level = str(level)
+                pro = str(pro)
+                if pro in sol:
+                    if level in sol[pro]:
                         heappop(promin)
                     else:
                         print(pro)
@@ -46,7 +52,9 @@ for _ in range(m):
                     print(pro)
                     break
 
-
     elif command[0] == 'solved': # 풀었으면 sol에 넣기
-        sol[command[1]] = '0'
-    print(sol)
+        if command[1] not in sol:
+            sol[command[1]] = []
+        sol[command[1]].append(lst[command[1]])
+        
+    # print(f"promin : {promin}, solved : {sol}")
