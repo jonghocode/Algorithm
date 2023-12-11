05beeed5c7715e0c6475b53ae8d564466399f4df
whdgh256 <whdgh256@naver.com>
2023-12-11 21:06:33 +0900

    Algorithm

--- practice.py
+++ practice.py
@@ -1,26 +1,52 @@
+from heapq import heappush, heappop
 import sys
-sys.setrecursionlimit(10**6)
-def dfs(v):
-    res = 0
-    for i in graph[v]:
-        res += dfs(i)
-    if lst[v][0] == 'S':
-        res += lst[v][1]
-    else:
-        res -= lst[v][1]
-        if res < 0:
-            res = 0
-    return res
-    
+
 n = int(input())
-graph = {i : [] for i in range(1, n+1)}
-lst = [[0, 0], [0, 0]]
+promax = [] # 최대힙
+promin = [] # 최소힙
+sol = {}
+for _ in range(n):
+    p, l = map(int, sys.stdin.readline().split())
+    heappush(promax, [-l, -p])
+    heappush(promin, [l, p])
 
-answer = 0
-for i in range(n-1):
-    what, cnt, num = map(str, sys.stdin.readline().strip().split())
-    cnt = int(cnt); num = int(num)
-    lst.append([what, cnt])
-    graph[num].append(i+2)
+m = int(input())
+for _ in range(m):
+    command = list(sys.stdin.readline().strip().split())
+    if command[0] == 'add': # 더하기
+        if command[1] in sol:
+            sol[command[1]] = command[2]
 
-print(dfs(1))
+        heappush(promax, [-int(command[2]), -int(command[1])])
+        heappush(promin, [int(command[2]), int(command[1])])
+
+    elif command[0] == 'recommend': # 추천하기
+        if command[1] == '1':
+            while True: # sol안에 없는 것을 뽑을 때 까지 뽑기
+                level, pro = -promax[0][0], -promax[0][1]
+                if level in sol:
+                    if sol[str(level)] == '0':
+                        heappop(promax)
+                    else:
+                        print(pro)
+                        break
+                else:
+                    print(pro)
+                    break
+        elif command[1] == '-1':
+            while True: # sol안에 없는 것을 뽑을 때 까지 뽑기
+                level, pro = promin[0][0], promin[0][1]
+                if level in sol:
+                    if sol[level] in pro:
+                        heappop(promin)
+                    else:
+                        print(pro)
+                        break
+                else:
+                    print(pro)
+                    break
+
+
+    elif command[0] == 'solved': # 풀었으면 sol에 넣기
+        sol[command[1]] = '0'
+    print(sol)
