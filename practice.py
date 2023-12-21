014eb9f02f375a4fd8bab7779b5c768191330c93
jonghocode <whdgh256@naver.com>
2023-12-21 20:54:08 +0900

    Algorithm

--- practice.py
+++ practice.py
@@ -1,34 +1,26 @@
-def dfs(now, color):
-    global sw
+# 3차원 dp
+# 0은 가로, 1은 세로, 2는 대각선
 
-    visit[now] = color
-    for go in graph[now]:
-        if visit[go] == -1:
-            next = 1 if color == 0 else 0
-            dfs(go, next)
-        elif visit[go] == color:
-            sw = 1
-            return
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+dp = [[[0]*3 for _ in range(n)] for _ in range(n)]
 
-t = int(input())
-for _ in range(t):
-    node, edge = map(int, input().split())
-    graph = {i : [] for i in range(1, node+1)}
-    visit = [-1 for _ in range(node+1)]
+# for i in range(n): # 초기화
+#     dp[0][i][0] = 1
+dp[0][1][0] = 1
+for i in range(2,n):
+    if board[0][i] == 0:
+        dp[0][i][0] = dp[0][i-1][0]
 
-    for _ in range(edge):
-        st, ed = map(int, input().split())
-        graph[st].append(ed)
-        graph[ed].append(st)
-    
-    sw = 0
-    for i in range(1, node+1):
-        if visit[i] == -1 and i in graph:
-            dfs(i, 0)
-            if sw == 1:
-                print("NO")
-                break
-    
-    if sw == 0:
-        print("YES")
-    
+for i in range(1, n):
+    for j in range(2, n):
+        if board[i][j] == 0:
+            if board[i-1][j] + board[i][j-1] == 0:
+                dp[i][j][2] = dp[i-1][j-1][2] + dp[i-1][j-1][1] + dp[i-1][j-1][0]
+            dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
+            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2]
+            
+
+# for i in range(n):
+#     print(dp[i])
+print(dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2])
