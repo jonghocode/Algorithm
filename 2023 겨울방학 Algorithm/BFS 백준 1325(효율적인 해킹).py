6dc3ae7aa41b7daf24851a9010974b6566b80a19
whdgh256 <whdgh256@naver.com>
2023-12-31 19:57:50 +0900

    Algorithm

--- 2023 겨울방학 Algorithm/BFS 백준 1325(효율적인 해킹).py
+++ 2023 겨울방학 Algorithm/BFS 백준 1325(효율적인 해킹).py
@@ -1,23 +1,17 @@
 import sys
 from collections import deque
 
-def bfs(k):
-    visit = [0 for _ in range(n+1)]
-    q = deque()
-    q.append(k)
-    visit[k] = 1
-    cnt = 1
-    while q:
-        now = q.popleft()
-        
-        for i in graph[now]:
-            if visit[i] == 0:
-                q.append(i)
-                visit[i] = 1
-                cnt += 1
-    
-    return cnt
+def dfs(idx):
+    if len(graph[idx]) == 0:
+        return 1
+    if dp[idx] != -1:
+        return dp[idx]
+    dp[idx] = 1
 
+    for go in graph[idx]:
+        dp[idx] += dfs(go)
+
+    return dp[idx]
 
 n, m = map(int, input().split())
 graph = {i : [] for i in range(1, n+1)}
@@ -25,16 +19,12 @@ for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
     graph[b].append(a)
 
-answer = []
+dp = [-1]*(n+1)
 for i in range(1, n+1):
-    if graph[i]:
-        answer.append([i, bfs(i)])
-answer.sort(key = lambda x : (-x[1], x[1]))
-t = answer[0][1]
-result = [answer[0][0]]
-for i in range(1, len(answer)):
-    if t != answer[i][1]:
-        break
-    result.append(answer[i][0])
-for i in range(len(result)):
-    print(result[i], end=' ')
+    if graph[i] and dp[i] == -1:
+        dfs(i)
+
+k = max(dp)
+for i in range(1, n+1):
+    if dp[i] == k:
+        print(i, end=' ')
