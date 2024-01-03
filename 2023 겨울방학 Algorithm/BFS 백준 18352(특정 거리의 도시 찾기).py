fed38e8547e7cf070d7e32a268b7f04d27f5f4a8
jonghocode <whdgh256@naver.com>
2024-01-03 09:23:57 +0900

    Algorithm

--- 2023 겨울방학 Algorithm/BFS 백준 18352(특정 거리의 도시 찾기).py
+++ 2023 겨울방학 Algorithm/BFS 백준 18352(특정 거리의 도시 찾기).py
@@ -3,9 +3,28 @@ from collections import deque
 
 n, m, k, x = map(int, input().split())
 graph = {i : [] for i in range(1, n+1)}
-visit = [0 for _ in range(n+1)]
+visit = [0x7fffffff for _ in range(n+1)]
 
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
 
+q = deque()
+q.append([x, 0])
+visit[x] = 0
+answer = 0
+while q:
+    now, d = q.popleft()
+    
+    for go in graph[now]:
+        if visit[go] > d + 1:
+            q.append([go, d+1])
+            visit[go] = d + 1
+
+for i in range(1, n+1):
+    if visit[i] == k:
+        print(i)
+        answer = 1
+
+if answer == 0:
+    print(-1)
