ff541edcd02d5c3ab7e6758ef468265f862cd8d4
jonghocode <whdgh256@naver.com>
2024-01-16 13:48:25 +0900

    Algorithm

--- test.py
+++ test.py
@@ -1,43 +1,34 @@
-from collections import deque
+def find(idx):
+    if root[idx] == idx:
+        return root[idx]
+    root[idx] = find(root[idx])
+    return root[idx]
 
-board = [list(map(int, input().split())) for _ in range(3)]
-s, X = 0, 123777
-hash = [[] for _ in range(X)]
-dir = [[1, 3], [0, 2, 4], [1, 5], [0, 4, 6], [1, 3, 5, 7], [2, 4, 8], [3, 7], [4, 6, 8], [5, 7]]
+n, m = map(int, input().split())
+bad = list(map(int, input().split()))
+bad = bad[1:]
+party = []
+for _ in range(m):
+    temp = list(map(int, input().split()))
+    party.append(temp[1:])
 
-for i in range(3):
-    for j in range(3):
-        s = s*10 + board[i][j]
+root = [i for i in range(n+1)]
+for i in range(len(bad)-1):
+    n1, n2 = find(bad[i]), find(bad[i+1])
+    if n1 != n2:
+        root[n2] = n1
 
-q = deque([(s, 0)])
-hash[s%X].append(s)
+for lst in party:
+    for i in range(len(lst)-1):
+        n1, n2 = find(lst[i]), find(lst[i+1])
+        if n1 != n2:
+            root[n2] = n1
 
-while q:
-    now, d = q.popleft()
-    now = str(now)
-    k = now.find('0')
-    
-    if now == '123456780':
-        print(d)
-        exit()
+answer = 0
+n1 = find(bad[0])
+for lst in party:
+    n2 = find(lst[0])
+    if n1 != n2:
+        answer += 1
 
-    for i in range(len(dir[k])):
-        temp = ''
-        for j in range(9):
-            if j == k:
-                temp += now[dir[k][i]]
-            elif j == dir[k][i]:
-                temp += now[k]
-            else:
-                temp += now[j]
-
-        sw = 0
-        for j in range(len(hash[int(temp)%X])):
-            if hash[int(temp)%X][j] == temp:
-                sw = 1
-                break
-        if sw == 0:
-            hash[int(temp)%X].append(temp)
-            q.append((temp, d+1))
-
-print(-1)
+print(answer)
