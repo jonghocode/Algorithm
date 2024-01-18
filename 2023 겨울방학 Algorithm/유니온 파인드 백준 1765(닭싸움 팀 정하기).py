99d3b0cdc36a72ae87324cf93160f30fefeb32fe
jonghocode <whdgh256@naver.com>
2024-01-18 13:47:18 +0900

    Algorithm

--- 2023 겨울방학 Algorithm/유니온 파인드 백준 1765(닭싸움 팀 정하기).py
+++ 2023 겨울방학 Algorithm/유니온 파인드 백준 1765(닭싸움 팀 정하기).py
@@ -1,30 +1,54 @@
 import sys
-from collections import defaultdict
 
-def find(x):
-    if root[x] == x:
-        return root[x]
-    root[x] = find(root[x])
-    return root[x]
+def find(idx): # 부모 찾기
+    if root[idx] == idx:
+        return root[idx]
+    root[idx] = find(root[idx])
+    return root[idx]
+
+def dfs(idx, type): # 원수의 원수는 친구이기 때문에 이분그래프 처럼 나누기
+    if visit[idx] != 0:
+        return
+    visit[idx] = type
+    type *= -1
+    for v in graph[idx]:
+        if visit[v] == 0:
+            dfs(v, type)
 
 n = int(input())
 m = int(input())
-E, F = [], []
+root = [i for i in range(n+1)]
+graph = {i : [] for i in range(1, n+1)} # 원수 그래프 생성
 
 for _ in range(m):
-    k = list((sys.stdin.readline().strip().split()))
-    a, b = int(k[1]), int(k[2])
-    if k[0] == 'E':
-        E.append((a, b))
-        E.append((b, a))
+    lst = list(map(str, sys.stdin.readline().strip().split()))
+    k, a, b = lst[0], int(lst[1]), int(lst[2])
+    if k == 'F': # 친구라면 바로 묶어주기
+        n1, n2 = find(a), find(b)
+        if n1 != n2:
+            root[n2] = n1
     else:
-        F.append((a, b))
-        F.append((b, a))
+        graph[a].append(b)
+        graph[b].append(a)
 
-root = [i for i in range(n+1)]
-for st, ed in F:
-    a, b = find(st), find(ed)
-    if a != b:
-        root[b] = a
+visit = [0 for i in range(n+1)]
+cnt = -1
+for i in range(1, n+1):
+    if visit[i] == 0 and len(graph[i]): # 원수 그래프 만들기
+        dfs(i, cnt)
+        cnt -= 1
 
-print(root)
+chk = [0 for i in range(n+1)]
+for i in range(1, n+1): # 친구 그래프와 합쳐주기
+    if visit[i] == 0 or chk[i] == 1: continue
+    for j in range(n+1):
+        if visit[i] == visit[j] and chk[j] == 0:
+            n1, n2 = find(i), find(j)
+            if n1 != n2:
+                root[n2] = n1
+            chk[i] = 1; chk[j] = 1
+
+answer = set()
+for i in range(1, n+1):
+    answer.add(find(i))
+print(len(answer))
