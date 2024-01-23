e22dc7c801698fe2b2cb5fc47b5f66a43b9de8a9
jonghocode <whdgh256@naver.com>
2024-01-23 12:41:21 +0900

    Algorithm

--- 2023 겨울방학 Algorithm/위상 정렬 백준 1948(임계경로).py
+++ 2023 겨울방학 Algorithm/위상 정렬 백준 1948(임계경로).py
@@ -6,36 +6,41 @@ m = int(input())
 graph = {i : [] for i in range(n+1)}
 remove_graph = {i : [] for i in range(n+1)}
 chk = [0 for _ in range(n+1)]
-dp = [0 for _ in range(n+1)]
 
-for _ in range(m):
-    a, b, c = map(int, sys.stdin.readline().split())
-    graph[a].append((b, c))
-    remove_graph[b].append((a, c))
+for i in range(m):
+    a, b, t = map(int, sys.stdin.readline().split())
+    graph[a].append((b, t))
+    remove_graph[b].append((a, t))
     chk[b] += 1
 
 st, ed = map(int, input().split())
-q = deque([(st, 1)])
-chk[st] -= 1
+q = deque()
+for i in range(1, n+1):
+    if chk[i] == 0:
+        q.append(i)
+
+dp = [0 for _ in range(n+1)]
 while q:
-    now, d = q.popleft()
+    now = q.popleft()
 
     for e, w in graph[now]:
         if chk[e] > 0:
             chk[e] -= 1
             if chk[e] == 0:
-                q.append((e, d+1))
-                chk[e] -= 1
-        
-            dp[e] = max(dp[e], w + dp[now])
+                q.append(e)
+            dp[e] = max(dp[e], dp[now] + w)
 
-q = deque([(ed, 1)])
-while q:
-    now, d = q.popleft()
-
+# ed애서 출발해서 st까지
+Q = deque([(ed)])
+chk = set()
+answer = 0
+while Q: # 최장경로 역추적 -> 처음에 2차원 리스트로 방문체크를 했는데 4800ms정도의 시간이 걸렸는데 딕셔너리와 chk를 사용하니 시간이 300ms로 줄었고 메모리 소모도 많이 줄었다.
+    now = Q.popleft()
     for e, w in remove_graph[now]:
-        if dp[now] == dp[e] + w:
-            q.append((e, d+1))
+        if dp[now] == dp[e] + w and (now, e) not in chk:
+            Q.append(e)
+            chk.add((now, e))
+            answer += 1
 
 print(dp[ed])
-print(d+1)
+print(answer)
