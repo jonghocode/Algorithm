c09cc640bb056a6de775c8bf72f492dc64470990
jonghocode <whdgh256@naver.com>
2024-01-30 14:34:09 +0900

    Algorithm

--- 2023 겨울방학 Algorithm/백준 11437(LCA).py
+++ 2023 겨울방학 Algorithm/백준 11437(LCA).py
@@ -1,29 +1,15 @@
 import sys
+from collections import deque
 
-def saveDepth(before, root, d):
-    depth[root][0], depth[root][1] = d, before
-    for node in graph[root]:
-        if depth[node][0] == -1:
-            saveDepth(root, node, d+1)
+def bfs():
+    q = deque([(0, 1, 0)])
 
-def findRoot(n1, n2, d1, d2):
-    if depth[n1][1] == n2:
-        print(n2)
-        return
-    elif depth[n2][1] == n1:
-        print(n1)
-        return
-        
-    if d1 > d2:
-        findRoot(depth[n1][1], n2, d1-1, d2)
-    elif d1 < d2:
-        findRoot(n1, depth[n2][1], d1, d2-1)
-    else:
-        if depth[n1][1] == depth[n2][1]:
-            print(depth[n1][1])
-            return
-        else:
-            findRoot(depth[n1][1], depth[n2][1], d1-1, d2-1)
+    while q:
+        before, root, d = q.popleft()
+        parent[root][0], parent[root][1] = before, d
+        for node in graph[root]:
+            if parent[node][0] == -1:
+                q.append((root, node, d+1))
 
 n = int(input())
 graph = {i : [] for i in range(n+1)}
@@ -32,10 +18,20 @@ for _ in range(n-1):
     graph[a].append(b)
     graph[b].append(a)
 
-depth = [[-1 for _ in range(2)] for _ in range(n+1)]
-saveDepth(0, 1, 0)
+parent = [[-1, -1] for _ in range(n+1)]
+bfs()
 
 m = int(input())
 for _ in range(m):
     n1, n2 = map(int, sys.stdin.readline().split())
-    findRoot(n1, n2, depth[n1][0], depth[n2][0])
+
+    while True:
+        if n1 == n2:
+            print(n1)
+            break
+        if parent[n1][1] < parent[n2][1]:
+            n2 = parent[n2][0]
+        elif parent[n1][1] > parent[n2][1]:
+            n1 = parent[n1][0]
+        else:
+            n1, n2 = parent[n1][0], parent[n2][0]
