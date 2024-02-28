95e07df850cbb34d3203a047d729f8288874ec9e
jonghocode <whdgh256@naver.com>
2024-02-28 11:28:09 +0900

    Algorithm

--- test.py
+++ test.py
@@ -1,49 +1,29 @@
-# 1738번 골목길(O)
+# 1956번 운동(O)
 import sys
-from collections import deque
 input = sys.stdin.readline
-
-def p(idx):
-    if rev[idx] == -1:
-        return
-    p(rev[idx])
-    print(idx)
-
 
 n, m = map(int, input().split())
 MAX = int(1e12)
-graph = []
-rev_graph = {i : [] for i in range(n+1)}
+MAP = [[MAX for _ in range(n+1)] for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            MAP[i][j] = 0
+
 for i in range(m):
     a, b, c = map(int, input().split())
-    graph.append((a, b, c))
-    rev_graph[b].append(a)
+    MAP[a][b] = c
 
-q = deque([(n)])
-chk = [0 for _ in range(n+1)]; chk[n] = 1
-while q:
-    now = q.popleft()
-    for e in rev_graph[now]:
-        if chk[e] == 0:
-            q.append(e)
-            chk[e] = 1
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            MAP[i][j] = min(MAP[i][j], MAP[i][k] + MAP[k][j])
 
-dis = [-MAX for _ in range(n+1)]; dis[1] = 0
-rev = [-1 for _ in range(n+1)]
-
+answer = MAX
 for i in range(1, n+1):
-    sw = 0
-    for temp in graph:
-        a, b, c = temp
-        if dis[a] == -MAX:
+    for j in range(1, n+1):
+        if i == j:
             continue
-        if dis[b] < dis[a] + c:
-            dis[b] = dis[a] + c
-            rev[b] = a
-            if chk[a] == 1:
-                sw = 1
+        answer = min(answer, MAP[i][j] + MAP[j][i])
 
-if sw == 0:
-    print(p(n))
-else:
-    print(-1)
+print(answer if answer != MAX else -1)
