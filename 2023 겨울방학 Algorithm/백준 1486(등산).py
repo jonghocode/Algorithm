62dc62df8a7e78e7e6b0847cd77775d30eaa359e
jonghocode <whdgh256@naver.com>
2024-03-06 18:28:55 +0900

    Algorithm

--- 2023 겨울방학 Algorithm/백준 1486(등산).py
+++ 2023 겨울방학 Algorithm/백준 1486(등산).py
@@ -3,8 +3,8 @@ input = sys.stdin.readline
 
 n, m, t, d = map(int, input().split())
 board = [list(input().strip()) for _ in range(n)]
-chk = [[0 for _ in range(m)] for _ in range(n)]
 MAX = int(1e12)
+chk = [[0 for _ in range(m)] for _ in range(n)]
 dis = [MAX for _ in range(n*m+1)]; dis[1] = 0
 graph = []
 dirx, diry = [-1, 1, 0, 0], [0, 0, -1, 1]
@@ -24,6 +24,11 @@ for i in range(n):
 
 for i in range(n):
     for j in range(m):
+        if i == j:
+            chk[i][j] = 0
+
+for i in range(n):
+    for j in range(m):
         for k in range(4):
             x, y = i + dirx[k], j + diry[k]
             if x < 0 or x >= n or y < 0 or y >= m:
@@ -32,34 +37,30 @@ for i in range(n):
             if time > t:
                 continue
             if board[i][j] >= board[x][y]:
-                graph.append((chk[i][j], chk[x][y], 1))
+                chk[x][y] = 1
             else:
-                graph.append((chk[i][j], chk[x][y], time**2))
+                chk[x][y] = time**2
 
-graph2 = []
-answer = 0
-for i in range(n):
-    for temp in graph:
-        a, b, c = temp
-        if dis[a] == MAX:
-            continue
-        if dis[b] > dis[a] + c and dis[a] + c <= d:
-            dis[b] = dis[a] + c
-            graph2.append(a)
-            graph2.append(b)
-
-for k in graph2:
+for k in range(n):
     for i in range(n):
         for j in range(m):
-            if chk[i][j] == k:
-                answer = max(answer, board[i][j])
+            chk[i][j] = min(chk[i][j], chk[i][k] + chk[k][j])
 
 print(dis)
-print(answer)
 
 for i in range(n):
     print(board[i])
 for i in range(n):
     print(chk[i])
-for i in graph:
-    print(i)
+
+answer = 0
+for i in range(n):
+    for j in range(n):
+        if chk[0][j] == MAX or chk[j][0] == MAX:
+            continue
+        if i == j:
+            continue
+        if chk[i][j] + chk[j][i] <= d and answer < board[i][j]:
+            answer = board[i][j]
+
+print(answer)
