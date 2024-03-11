d2570bc3c61cc7b392c43c30d24ed4c8e80644fb
jonghocode <whdgh256@naver.com>
2024-03-11 17:57:43 +0900

    Algorithm

--- Greedy/백준 13904(과제).py
+++ Greedy/백준 13904(과제).py
@@ -1,3 +1,5 @@
+# 하루에 한 문제만 가능 -> 과제를 해야하는 날짜 중 가장 긴 날짜를 기준으로 그 날짜 까지 하루 마다 제일 높은 것만 기록하면 됨
+
 import sys
 input = sys.stdin.readline
 
@@ -9,7 +11,7 @@ MAX = int(1e12)
 
 for d, w in lst:
     sw = MAX
-    for i in range(1, d+1):
+    for i in range(1, d+1): # 1일부터 과제를 제출해야하는 날짜까지 최솟값을 제일 높은 값으로 교체하기
         if dp[i] < sw:
             sw = dp[i]
             idx = i
@@ -17,5 +19,3 @@ for d, w in lst:
     dp[idx] = w
 
 print(sum(dp))
-for i in range(n+1):
-    print(dp[i], end=' ')
